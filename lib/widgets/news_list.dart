0ee28fd8c9f72909883396318baf20074e2f8c86
Shivansh Singh <me.shivansh007@gmail.com>
2022-08-13 14:03:37 +0530

    news list

--- lib/widgets/news_list.dart
+++ lib/widgets/news_list.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:newsapp_project/models/news_model.dart';
 
@@ -12,11 +13,93 @@ class NewsList extends StatefulWidget {
 class _NewsListState extends State<NewsList> {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: widget.articles.length,
-      itemBuilder: ((context, index) {
-        return Text(widget.articles[index].title!);
-      }),
+    return Container(
+      margin: const EdgeInsets.only(top: 16, bottom: 5),
+      child: ListView.builder(
+        itemCount: widget.articles.length,
+        itemBuilder: ((context, index) {
+          return GestureDetector(
+            onTap: () {},
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width,
+              child: widget.articles[index].urlToImage != null
+                  ? Container(
+                      margin: const EdgeInsets.symmetric(horizontal: 16),
+                      padding: const EdgeInsets.only(bottom: 24),
+                      child: Stack(
+                        alignment: Alignment.bottomCenter,
+                        children: [
+                          ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: CachedNetworkImage(
+                              height: 250,
+                              width: double.infinity,
+                              imageUrl: widget.articles[index].urlToImage!,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                          Container(
+                            height: 130,
+                            decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.only(
+                              bottomLeft: Radius.circular(10),
+                              bottomRight: Radius.circular(10),
+                            )),
+                            child: Container(
+                              margin:
+                                  const EdgeInsets.symmetric(horizontal: 16),
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.end,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    widget.articles[index].title!,
+                                    overflow: TextOverflow.clip,
+                                    maxLines: 3,
+                                    style: const TextStyle(
+                                        color: Color(0xfff2f2f2),
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 20),
+                                  ),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    children: [
+                                      Text(
+                                        widget.articles[index].source!.name!,
+                                        style: const TextStyle(
+                                            color: Color(0xffbababa),
+                                            fontWeight: FontWeight.bold,
+                                            fontSize: 12),
+                                      ),
+                                      SizedBox(
+                                        width:
+                                            MediaQuery.of(context).size.width *
+                                                0.05,
+                                      ),
+                                      Text(
+                                        widget.articles[index].publishedAt!,
+                                        style: const TextStyle(
+                                            color: Color(0xffbababa),
+                                            fontSize: 11,
+                                            fontWeight: FontWeight.w100),
+                                      ),
+                                      const SizedBox(
+                                        height: 40,
+                                      )
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                    )
+                  : null,
+            ),
+          );
+        }),
+      ),
     );
   }
 }
